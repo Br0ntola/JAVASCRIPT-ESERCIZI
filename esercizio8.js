24410ee158622e75a7715acffe40ee50c7247bc8
valentina Li <valentinalidev@gmail.com>
2023-11-21 17:01:13 +0100

    added esercizio8

---
+++ esercizio8.js
@@ -0,0 +1,5 @@
+//Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+let myString = `Hi, this is my string,
+she is a long sentence formed with:
+BACKTICKS`
