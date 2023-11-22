0c1296d511c89ac59b85471f1cae7de881c8212d
valentina Li <valentinalidev@gmail.com>
2023-11-22 13:08:34 +0100

    modified esercizio2

--- ESERCIZI_RESTPARAMETER/esercizio2.js
+++ ESERCIZI_RESTPARAMETER/esercizio2.js
@@ -3,11 +3,8 @@ Utilizzare il parametro rest per raccogliere gli argomenti.*/
 
 //Prova ad usare l'operatore modulo per ottenere i numeri pari.
 
-const array = []
-
 function filterOutOdds(...numbers) {
     return numbers.filter(num => num % 2 == 0)
-
 };
 
 const myNum = filterOutOdds(1, 2, 3, 4, 5, 6);
