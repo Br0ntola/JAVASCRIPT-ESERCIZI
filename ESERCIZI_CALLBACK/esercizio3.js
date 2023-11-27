49edacbc30db602af80778606ed3e07f6e5b80c7
valentina Li <valentinalidev@gmail.com>
2023-11-27 13:46:54 +0100

    re added esercizi 1, 2, 3, 4

---
+++ ESERCIZI_CALLBACK/esercizio3.js
@@ -0,0 +1,19 @@
+/*Scrivi una funzione chiamata filterArray che accetta un array e una funzione di callback come argomenti. 
+-La funzione callback deve filtrare e restituire i numeri pari passati come argomento alla funzione filterArray
+filterArray dovrebbe restituire un nuovo array contenente solo gli elementi per i quali la funzione di callback restituisce true.*/
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+function filterArray(arr, callback) {
+    let nuovoArray = [];
+    nuovoArray = arr.filter(callback)
+    return nuovoArray
+};
+
+function isEven(num) {
+    return num % 2 === 0
+
+};
+
+const evenNumbers = filterArray(numbers, isEven);
+console.log(evenNumbers); // Should output [2, 4, 6]
